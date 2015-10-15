45001f9aec1ef93c5b46030fbe261435c5a863aa
Michel Weststrate <mweststrate@gmail.com>
2015-10-15 10:29:20 +0200

    Added mobservable / mobservable-react to the project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React-TypeScript
 
-Minimal boilerplate for a single-page app using React, TypeScript 1.6 with TSX, and Visual Studio Code.
+Minimal boilerplate for a single-page app using Mobservable, Mobservable-React, React, TypeScript 1.6 with TSX, and Visual Studio Code.
 
 Usage:
 
@@ -15,3 +15,7 @@ To view the app:
 * `npm install -g http-server`
 * `http-server`
 * Open [http://localhost:8080/](http://localhost:8080/) in your browser of choice.
+
+## Credits
+
+This repo has been forked from the [React Typescript boilerplate](https://github.com/bvanreeven/react-typescript) repo.
--- app/app.tsx
+++ app/app.tsx
@@ -1,6 +1,8 @@
 /// <reference path="../typings/react/react.d.ts" />
 
 import React = require('react');
+import {observable} from 'mobservable';
+import {observer} from 'mobservable-react';
 
 class DemoProps {
   public name: string;
@@ -12,8 +14,27 @@ class Demo extends React.Component<DemoProps, any> {
   }
   render() {
     return (
-      <div>Hello {this.props.name}!</div>
+      <div>
+        <div>Hello {this.props.name}!</div>
+        <Timer/>
+      </div>
     );
+  }
+}
+
+
+var timerState = observable({
+  secondsPassed: 0
+});
+
+setInterval(() => timerState.secondsPassed++, 1000);
+
+@observer
+class Timer extends React.Component<{}, {}> {
+  render() {
+    return (
+      <span>Seconds passed: {timerState.secondsPassed}</span>
+    )
   }
 }
 
