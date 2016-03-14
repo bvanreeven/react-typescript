6c0878487fc91c2ff5886256176e4a72aaa79547
Michel Weststrate <mweststrate@gmail.com>
2016-03-14 10:27:34 +0100

    Disabled webpack HMR. See #2

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Initial run:
 * Install Node.js
 * `npm install`
 * `npm start`
+
+For simplicity sake Webpack Hot Module Reloading is disabled. If you want to use HMR, see the [Reactive2015 demo](https://github.com/mobxjs/mobx-reactive2015-demo) to see a valid setup.
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
@@ -13,17 +12,13 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
   resolve: {
     extensions: ['', '.js', '.ts', '.tsx']
   },
   module: {
     loaders: [{
       test: /\.tsx?$/,
-      loaders: ['react-hot', 'ts-loader'],
+      loaders: ['ts-loader'],
       include: path.join(__dirname, 'src')
     }]
   }
