1b55b2bfe78961468d998ce143546e371404c8f2
Benny van Reeven <bvanreeven@gmail.com>
2016-10-17 19:14:30 +0200

    Improvements
    * Normalize line endings.
    * Normalize indentation.
    * Normalize quotes.
    * Use stateless functional component.
    * Exclude `temp` dir from VSCode.

--- app/app.tsx
+++ app/app.tsx
@@ -1,26 +1,9 @@
-import React = require('react');
-import ReactDOM = require('react-dom');
+import React = require("react");
+import ReactDOM = require("react-dom");
 
-class DemoProps {
-  public name: string;
-}
+const Demo = ({name} : {name: string}) => <div>Hello {name}!</div>;
 
-class Demo extends React.Component<DemoProps, any> {
-  constructor(props: DemoProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>Hello {this.props.name}!</div>
-    );
-  }
-}
-
-function render() {
-  ReactDOM.render(
-    <Demo name="World" />,
-    document.getElementById('app')
-  );
-}
-
-render();
+ReactDOM.render(
+  <Demo name="World" />,
+  document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    entry: "./app/app.tsx",
-    output: {
-        filename: "./public/js/app/bundle.js",
-    },
+  entry: "./app/app.tsx",
+  output: {
+    filename: "./public/js/app/bundle.js",
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
 
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ],
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ],
 
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
 };
