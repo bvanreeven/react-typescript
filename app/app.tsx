84566e72f7150154f1f0bb742f78c1e9a7f22551
Benny van Reeven <bvanreeven@gmail.com>
2016-10-19 17:02:54 +0200

    Fix imports and show colors in webpack output

--- app/app.tsx
+++ app/app.tsx
@@ -1,5 +1,5 @@
-import React = require("react");
-import ReactDOM = require("react-dom");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 const Demo = ({name} : {name: string}) => <div>Hello {name}!</div>;
 
